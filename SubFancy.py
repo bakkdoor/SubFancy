065e6599ce42b43726dedc96939e629796a78037
Christopher Bertels <bakkdoor@flasht.de>
2012-07-10 17:50:51 +0200

    use threadlocals and whastnot

--- SubFancy.py
+++ SubFancy.py
@@ -1,20 +1,23 @@
-import sys
+import sys, threading
 import msgpackrpc
 
 DefaultHost = "127.0.0.1"
 DefaultPort = 52400
 
+LOCAL = threading.local()
+LOCAL.msgpack_client = None
+
 class Client:
   def __init__(self, host = DefaultHost, port = DefaultPort):
     self.host = host
     self.port = port
-    self.client = None
-    self.connect()
+    self.client = LOCAL.msgpack_client
+    if self.client == None:
+      self.connect()
 
   def connect(self):
-    if self.client == None:
-      self.client = msgpackrpc.Client(msgpackrpc.Address(self.host, self.port))
-    return self.client
+    LOCAL.msgpack_client = msgpackrpc.Client(msgpackrpc.Address(self.host, self.port))
+    self.client = LOCAL.msgpack_client
 
 
   def call(self, message, *args):
