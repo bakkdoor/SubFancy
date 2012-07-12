24330359196814d9cb7b0e0e6e0d31d45e603f15
Christopher Bertels <bakkdoor@flasht.de>
2012-07-12 15:09:23 +0200

    don't use thread locals, connected with each command instead

--- SubFancy.py
+++ SubFancy.py
@@ -4,23 +4,21 @@ import msgpackrpc
 DefaultHost = "127.0.0.1"
 DefaultPort = 52400
 
-LOCAL = threading.local()
-LOCAL.msgpack_client = None
-
 class Client:
   def __init__(self, host = DefaultHost, port = DefaultPort):
     self.host = host
     self.port = port
-    self.client = LOCAL.msgpack_client
-    if self.client == None:
-      self.connect()
+    self._client_ = None
+    self.connect()
 
   def connect(self):
-    LOCAL.msgpack_client = msgpackrpc.Client(msgpackrpc.Address(self.host, self.port))
-    self.client = LOCAL.msgpack_client
+     self._client_ = msgpackrpc.Client(msgpackrpc.Address(self.host, self.port))
+
+  def client(self):
+    if self._client_ == None:
+      self.connect()
+    return self._client_
 
 
   def call(self, message, *args):
-    if self.client == None:
-      self.connect()
-    return self.client.call(message, *args)
+    return self.client().call(message, *args)
